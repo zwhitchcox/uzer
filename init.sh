4221ad71c1fae8b164f9d8e58f821122399c8146
Zane Hitchcox <zwhitchcox@gmail.com>
2019-10-22 11:53:10 -0400

    add branch option

--- init.sh
+++ init.sh
@@ -2,9 +2,15 @@ echo "what is your library name?"
 read -r PROJ_NAME
 echo "what is your github username?"
 read -r GITHUB_USERNAME
+echo "Which branch would you like (react or master, default is master)?"
+read -r BRANCH
+
+if [ "$BRANCH" -ne "react" ]; then
+  BRANCH="master"
+fi
 
 echo "creating project $PROJ_NAME"
-git clone https://github.com/zwhitchcox/ts-lib $PROJ_NAME
+git clone --branch $BRANCH https://github.com/zwhitchcox/ts-lib $PROJ_NAME
 cd $PROJ_NAME
 sed -i -e 's/LIB_NAME/'$PROJ_NAME'/g' *
 sed -i -e 's/GITHUB_USERNAME/'$GITHUB_USERNAME'/g' *
